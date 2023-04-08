cf1f24d8bf5ab4da1f3ef8e51c7fa7ad10e9ee2b
Will Castaneda <will@vectorcare.com>
2023-04-08 16:22:42 -0700

    Mobile

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-gray-200 p-24">
-      <div className="grid grid-cols-6 gap-y-3 gap-x-2 w-2/6">
-        <div className="border-2 bg-white border-gray-600 col-span-6 py-20 text-lg font-extrabold text-center">
+    <main className="flex min-h-screen flex-col items-center bg-gray-200">
+      <div className="grid grid-cols-6 gap-y-3 gap-x-2 mt-8">
+        <div className="border-2 bg-white border-gray-600 col-span-6 py-24 px-6 text-lg font-extrabold text-center">
           The Never Bitcoin Pledge
         </div>
         <div className="py-2 border-2 bg-white border-gray-600 text-center col-span-6 font-semibold cursor-pointer hover:bg-gray-100">
