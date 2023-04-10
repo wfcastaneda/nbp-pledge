a28d1684c4487be5f1cc8a1bdc7d2965c6b0799d
Will Castaneda <will@vectorcare.com>
2023-04-09 17:01:09 -0700

    Revert to twitter v1.0 auth.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,9 +4,11 @@ import TwitterProvider from "next-auth/providers/twitter";
 export const authOptions = {
   providers: [
     TwitterProvider({
-        clientId: process.env.TWITTER_ID,
-        clientSecret: process.env.TWITTER_SECRET,
-        version: "2.0", // opt-in to Twitter OAuth 2.0
+        clientId: process.env.TWITTER_CLIENT_ID,
+        clientSecret: process.env.TWITTER_CLIENT_SECRET,
+        // clientId: process.env.TWITTER_ID,
+        // clientSecret: process.env.TWITTER_SECRET,
+        // version: "2.0", // opt-in to Twitter OAuth 2.0
       })
   ],
   secret: "PLACE-HERE-ANY-STRING",
